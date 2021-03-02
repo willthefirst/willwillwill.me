ba64c546ef12e3dde08c117052f468b7ce211d3e
Will Moritz <will@crowdfavorite.com>
2021-03-02 10:56:12 -0800

    first commit, connecting to postgres

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const db = require('./app/models');
+
+var corsOptions = {
+  origin: 'http://localhost:8081'
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Tables resynced.');
+});
+
+// simple route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to bezkoder application.' });
+});
+
+// Post routes
+require('./app/routes/post.routes.js')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
